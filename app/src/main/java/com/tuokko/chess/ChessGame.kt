c68d5976e12bd745350890a872b84c4ee19fada5
miro tuokko <miro.tuokko@student.tut.fi>
2020-11-22 16:30:25 +0200

    - Added a new activity that will contain the chess game

---
+++ app/src/main/java/com/tuokko/chess/ChessGame.kt
@@ -0,0 +1,13 @@
+package com.tuokko.chess
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ChessGame: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.chess_game_activity)
+    }
+}
--- app/src/main/java/com/tuokko/chess/MainActivity.kt
+++ app/src/main/java/com/tuokko/chess/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.tuokko.chess
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val newGameButton = findViewById<TextView>(R.id.start_new_game_button)
+        newGameButton.setOnClickListener {
+            val newGameIntent = Intent(this, ChessGame::class.java)
+            startActivity(newGameIntent)
+        }
     }
 }
